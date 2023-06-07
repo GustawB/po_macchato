fedb7794b9895596255324df318956c23c8de4bd
GustawB <gustaw.blachowski@gmail.com>
2023-06-07 14:09:07 +0200

    UPDATE Add tests for builders (Forgot to include tests xD).
    As the title says, this commit adds tests for builders. Additionally, Debugger has received an overloaded ru() that takea additional runMode parameter to omit the question about the debug mode and other unnecessary prints. Procedure class will receive its tests in a different branch because it's a whole new construct in the language.

---
+++ test/BuildersTests.java
@@ -0,0 +1,98 @@
+import expressions.Literal;
+import expressions.Variable;
+import expressions.Add;
+import expressions.Divide;
+import expressions.Literal;
+import expressions.Modulo;
+import expressions.Multiply;
+import expressions.Substract;
+import expressions.Variable;
+import expressions.Operations;
+import instructions.Block;
+import instructions.ForLoop;
+import instructions.IfStatement;
+import instructions.PrintExpr;
+import org.junit.jupiter.api.*;
+import runtime.Debugger;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BuildersTests {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+
+    //Tests basic functionality of builders in Block
+    @Test
+    public void TestBlockPrints(){
+        Block program = new Block.Builder()
+                .declareVariable('x', Literal.of(69))
+                .declareVariable('y', Literal.of(420))
+                .print(Variable.of('x'))
+                .print(Variable.of('y'))
+                .declareVariable('z', Literal.of(2137))
+                .print(Variable.of('z'))
+                .build();
+        Debugger debugger = new Debugger();
+        debugger.run(program, 'n');
+
+        //This type of test has problems with line separators, so I ignore them.
+        assertEquals("694202137", outputStreamCaptor
+                .toString().trim().replace("\n", "")
+                .replace("\r", ""));
+    }
+
+    //Tests builder functionality on Loops
+    @Test
+    public void TestLoopPrints(){
+        Block program = new Block.Builder()
+                .newLoop(new ForLoop.Builder('x', Literal.of(5))
+                        .print(Variable.of('x'))
+                        .assign('x', Add.of(Variable.of('x'), Literal.of(2)))
+                        .print(Variable.of('x'))
+                        .build())
+                .build();
+        Debugger debugger = new Debugger();
+        debugger.run(program, 'n');
+
+        assertEquals("0213243546", outputStreamCaptor
+                .toString().trim().replace("\n", "")
+                .replace("\r", ""));
+    }
+
+    //Tests builder functionality on IfStatements
+    @Test
+    public void TestIfStatements(){
+        Block program = new Block.Builder()
+                .newLoop(new ForLoop.Builder('x', Literal.of(10))
+                        .condition(new IfStatement
+                                .Builder(Modulo.of(Variable.of('x'),
+                                Literal.of(3)),
+                                Literal.of(0),
+                                Operations.EQUAL)
+                                .print(Variable.of('x'))
+                                .switchToElseBranch()
+                                .print(Literal.of(69))
+                                .build())
+                        .build())
+                .build();
+        Debugger debugger = new Debugger();
+        debugger.run(program, 'n');
+
+        assertEquals("0696936969669699", outputStreamCaptor
+                .toString().trim().replace("\n", "")
+                .replace("\r", ""));
+    }
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+}
