69344231da7b28294d833e61d0f1edeb94f28f9a
GustawB <gustaw.blachowski@gmail.com>
2023-06-09 18:45:34 +0200

    Add tests for procedures.
    As the title says, I added two tests for the procedures. The first one tests a basic functionality of printing a variable and a literal, and the second one tests the recursion.

---
+++ test/ProceduresTests.java
@@ -0,0 +1,82 @@
+import expressions.Add;
+import expressions.Divide;
+import expressions.Literal;
+import expressions.Modulo;
+import expressions.Multiply;
+import expressions.Operations;
+import expressions.Substract;
+import expressions.Variable;
+import instructions.Block;
+import instructions.IfStatement;
+import instructions.Procedure;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import runtime.Debugger;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProceduresTests {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    //This test tests the basic procedure functionality by
+    //implementing the example given in the instruction
+    @Test
+    public void BasicUsage(){
+        Block program = new Block.Builder()
+                .declareVariable('x', Literal.of(57))
+                .declareVariable('y', Literal.of(15))
+                .declareProcedure(new Procedure.declarationBuilder("out", 'a')
+                        .print(Variable.of('a'))
+                        .build())
+                .assign('x', Substract.of(Variable.of('x'), Variable.of('y')))
+                .invoke(new Procedure.invokeBuilder("out", Variable.of('x'))
+                        .build())
+                .invoke(new Procedure.invokeBuilder("out", Literal.of(125))
+                        .build())
+                .build();
+        Debugger debugger = new Debugger();
+        debugger.run(program, 'n');
+
+        assertEquals("42125", outputStreamCaptor
+                .toString().trim().replace("\n", "")
+                .replace("\r", ""));
+    }
+
+    @Test
+    public void recursion(){
+        Block program = new Block.Builder()
+                .declareProcedure(new Procedure.declarationBuilder("zegz", 'x')
+                        .condition(new IfStatement.Builder(Variable.of('x'), Literal.of(6), Operations.SMALLER_EQUAL)
+                                .print(Variable.of('x'))
+                                .assign('x', Add.of(Variable.of('x'), Literal.of(1)))
+                                .invoke(new Procedure.invokeBuilder("zegz", Variable.of('x'))
+                                        .build())
+                                .build())
+                        .build())
+                .invoke(new Procedure.invokeBuilder("zegz", Literal.of(0))
+                        .build())
+                .build();
+
+        Debugger debugger = new Debugger();
+        debugger.run(program, 'n');
+
+        assertEquals("0123456", outputStreamCaptor
+                .toString().trim().replace("\n", "")
+                .replace("\r", ""));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+}
